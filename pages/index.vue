40ea2cfc305a4cb9a64e59e43b520982ff9003f7
hiromi-mi <hiromi-mi@cat.zaq.jp>
2020-04-19 16:42:07 +0900

    fix link

--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,13 @@
         Portfolio Page of hiromi-mi
       </h2>
       <div class="links">
+      <!--
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
         </a>
+        -->
         <a
-          href="https://github.com/nuxt/nuxt.js"
+          href="https://github.com/hiromi-mi"
           target="_blank"
           class="button--grey"
         >
